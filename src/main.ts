6a3c7e23faa0ec98edb5655ac5f8871811dacf60
Kyryl Pozdniakov <kyryl.pozdniakov@itsua.eu>
2025-11-08 14:07:04 +0200

    added cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('Calories API')
     .setDescription('The Calories API description')
@@ -14,6 +16,30 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document, { useGlobalPrefix: true });
+
+  // CORS
+  const allowedOrigins = process.env.ALLOWED_ORIGINS
+    ? process.env.ALLOWED_ORIGINS.split(',')
+    : [];
+  const corsOptions: CorsOptions = {
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        console.log('CORS allowed:', origin);
+        callback(null, true);
+      } else {
+        console.log('Origin not allowed:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  };
+  app.enableCors(corsOptions);
 
   await app.listen(PORT, () =>
     console.log(`Server started on  http://localhost:${PORT}`),
